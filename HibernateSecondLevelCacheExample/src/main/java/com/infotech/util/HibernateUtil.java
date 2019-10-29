00b4f7a25c7eda42e67b15ca13b6b083d7c800a3
traju <traju@sys-11.softhink.com>
2019-10-29 02:00:36 -0400

    er

---
+++ HibernateSecondLevelCacheExample/src/main/java/com/infotech/util/HibernateUtil.java
@@ -0,0 +1,42 @@
+package com.infotech.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+/**
+ * @author
+ */
+public class HibernateUtil {
+  private static StandardServiceRegistry standardServiceRegistry;
+  private static SessionFactory sessionFactory;
+
+  static{
+	    if (sessionFactory == null) {
+	      try {
+	        // Create StandardServiceRegistry
+	    	// configures settings from hibernate.cfg.xml
+	        standardServiceRegistry = new StandardServiceRegistryBuilder()
+	            .configure()
+	            .build();
+	        // Create MetadataSources
+	        MetadataSources metadataSources = new MetadataSources(standardServiceRegistry);
+	        // Create Metadata
+	        Metadata metadata = metadataSources.getMetadataBuilder().build();
+	        // Create SessionFactory
+	        sessionFactory = metadata.getSessionFactoryBuilder().build();
+	      } catch (Exception e) {
+	        e.printStackTrace();
+	        if (standardServiceRegistry != null) {
+	          StandardServiceRegistryBuilder.destroy(standardServiceRegistry);
+	        }
+	      }
+	    }
+  }
+  //Utility method to return SessionFactory
+  public static SessionFactory getSessionFactory() {
+	  return sessionFactory;
+  }
+}
